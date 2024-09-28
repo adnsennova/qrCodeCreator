c6aa80ff2a785579fa3364b1f74fcebc8e6a4179
Stiven Colorado <stivenchoo@gmail.com>
2024-09-28 18:56:13 -0500

    add views and fix routes in frontend

---
+++ frontend/src/controller/home.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    console.log("se cargo");
+    const name_ = document.querySelector("#name_profile");
+    name_.textContent = "stiven Colorado"
+})
